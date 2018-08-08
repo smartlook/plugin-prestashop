c1ce0e8cad5b731ecc0a6a50533812c8bc705bd6
Marek Gach <gach@kurzor.net>
2018-08-08 16:42:51 +0200

    Change version to 2.0.1 in main class

--- smartlook/smartlook.php
+++ smartlook/smartlook.php
@@ -29,7 +29,7 @@ class Smartlook extends Module
     {
         $this->name = 'smartlook';
         $this->tab = 'advertising_marketing';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'Smartsupp';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
